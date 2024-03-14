6a4478c443d2cb1f658325c82de2baff859a9028
itonix <59088641+itonix@users.noreply.github.com>
2024-03-14 08:24:11 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,14 @@
+
+
+for (var i = 0;i < document.querySelectorAll(".drum").length; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+      console.log(this);
+    
+        //var audio = new Audio('sounds/tom-4.mp3');
+        // audio.play(); 
+    
+    
+});
+}
+
